7486dd0597e935410381e4198cebd9d8ecadd2c3
arpan <arpankumar.dutta@wisseminfotech.com>
2023-04-11 16:32:09 +0530

    login component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/login/Login';
 
 function App() {
   return (
     <div className="App">
       <h2>Shopping App Loading...</h2>
+      <Login />
     </div>
   );
+  
 }
 
 export default App;
